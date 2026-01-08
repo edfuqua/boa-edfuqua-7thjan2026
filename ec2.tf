0ae11a0c02a00f18f45a11518df0b55b9ecd1b8f
jbooysetx <johan.booyse@gmail.com>
2026-01-08 17:33:37 +0000

    ec2 with security group

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "example" {
   ami           = "var.edf-vm1"
   instance_type = "var.vm-size"
+  # attaching security group to ec2 vm
+  vpc_security_group_ids = [ aws_security_group.allow_tls.id]
 
   tags = {
     Name = "var.vm-name"
